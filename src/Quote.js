9c72721ab72c4c1d661c65b5e25acdaf7dd48c10
Joe LoPresti <joe@scientiamobile.com>
2019-03-28 15:57:32 -0400

    Linting to Airbnb standards

--- src/Quote.js
+++ src/Quote.js
@@ -1,19 +1,52 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Quote = (props) => {
-  const { quote, author, email, date, generateQuote, shareQuote } = props;
-  return ( 
+  const {
+    quote, author, email, date, generateQuote, shareQuote,
+  } = props;
+  return (
     <div className="box">
-      <p><strong>Quote:</strong> {quote}</p>
-      <p><strong>Author:</strong> {author}</p>
-      <p><strong>Email:</strong> {email}</p>
-      <p><strong>Date:</strong> {date}</p>
+      <p>
+        <strong>Quote:</strong>
+        {' '}
+        {quote}
+      </p>
+      <p>
+        <strong>Author:</strong>
+        {' '}
+        {author}
+      </p>
+      <p>
+        <strong>Email:</strong>
+        {' '}
+        {email}
+      </p>
+      <p>
+        <strong>Date:</strong>
+        {' '}
+        {date}
+      </p>
       <br />
-      <div className="buttons"><button className="button is-primary is-small" onClick={generateQuote}>New Quote</button>
-      <button className="button is-success is-small" onClick={shareQuote}>Tweet Quote</button></div>
-      
+      <div className="buttons">
+        <button className="button is-primary is-small" onClick={generateQuote}>New Quote</button>
+        <button className="button is-success is-small" onClick={shareQuote}>Tweet Quote</button>
+      </div>
+
     </div>
-   );
-}
- 
+  );
+};
+
+Quote.propTypes = {
+  quote: PropTypes.string,
+  author: PropTypes.string,
+  email: PropTypes.string,
+  date: PropTypes.string,
+  generateQuote: PropTypes.func,
+  shareQuote: PropTypes.func,
+};
+
 export default Quote;
--- src/QuoteCard.js
+++ src/QuoteCard.js
@@ -1,34 +1,50 @@
+/* eslint-disable react/button-has-type */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/require-default-props */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const QuoteCard = (props) => {
-    const { author, email, date, quote, shareQuote, deleteQuote } = props;
-  return ( 
-      <div className="box" key={date}>
+  const {
+    author, email, date, quote, shareQuote, deleteQuote,
+  } = props;
+  return (
+    <div className="box" key={date}>
       <article className="media">
-    <div className="media-left">
-      <figure className="image is-128x128">
-        <img src="https://bulma.io/images/placeholders/128x128.png" alt="Mock" />
-      </figure>
-    </div>
-    <div className="media-content">
-      <div className="content">
-        <p>
-          <strong>{author}</strong> <small>{email}</small> 
-          <br />
-          <small><em>{date}</em></small>
-          <br />
-          {quote}
-        </p>
+        <div className="media-left">
+          <figure className="image is-128x128">
+            <img src="https://bulma.io/images/placeholders/128x128.png" alt="Mock" />
+          </figure>
+        </div>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{author}</strong>
+              <small>{email}</small>
+              <br />
+              <small><em>{date}</em></small>
+              <br />
+              {quote}
+            </p>
+          </div>
+        </div>
+      </article>
+      <br />
+      <div className="buttons">
+        <button className="button is-success is-small" onClick={shareQuote}>Tweet Quote</button>
+        <button className="button is-danger is-small" onClick={deleteQuote}>Delete Quote</button>
       </div>
     </div>
-  </article>
-  <br />
-  <div className="buttons">
- <button className="button is-success is-small" onClick={shareQuote}>Tweet Quote</button>
- <button className="button is-danger is-small" onClick={deleteQuote}>Delete Quote</button>
-  </div>
-      </div>
-   );
-}
- 
+  );
+};
+
+QuoteCard.propTypes = {
+  quote: PropTypes.string,
+  author: PropTypes.string,
+  email: PropTypes.string,
+  date: PropTypes.string,
+  deleteQuote: PropTypes.func,
+  shareQuote: PropTypes.func,
+};
+
 export default QuoteCard;
