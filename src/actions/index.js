cbc0febf073027d87a8d94531cefb477f74904e4
Joe LoPresti <joe@scientiamobile.com>
2019-04-09 16:20:09 -0400

    Push generateQuote function over to actions in redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,15 @@
 import { GENERATE_QUOTE, DELETE_QUOTE } from './types';
 
-export const generateQuote = (payload) => {
-  type: 'GENERATE_QUOTE',
-  payload
+export const generateQuote = () => dispatch => {
+  const randomNum = Math.floor(Math.random() * (500 - 1)) + 1;
+  fetch(`https://jsonplaceholder.typicode.com/comments?id=${randomNum.toString()}`)
+  .then(res => res.json())
+  .then(data => {
+    dispatch({type: GENERATE_QUOTE, payload: data})
+  }
 }
 
 export const deleteQuote = index => {
-  type: 'DELETE_QUOTE',
-  index
+  type: DELETE_QUOTE,
+  payload: index
 }
